6906f6d6c70e6cf5512067925112ebe009ea234b
longnktx <yh_long.kotex@yahoo.com.vn>
2017-03-01 10:57:47 +0100

    Kruskalalalalalal

--- src/DirectedGraph.java
+++ src/DirectedGraph.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class DirectedGraph<E extends Edge> {
 
 	private int nbrOfNodes;
-	private List<Edge>[] edges;
+	private List<E>[] edges;
 
 	public DirectedGraph(int noOfNodes) {
 		nbrOfNodes = noOfNodes;
@@ -16,15 +16,59 @@ public class DirectedGraph<E extends Edge> {
 
 	public void addEdge(E e) {
 		edges[e.getSource()].add(e);
-	}
+    }
 
 	public Iterator<E> shortestPath(int from, int to) {
 		return null;
 	}
 		
-	public Iterator<E> minimumSpanningTree() {
-		return null;
-	}
+	public Iterator<E> minimumSpanningTree()
+    {
 
+        LinkedList<E>[] cc = new LinkedList[nbrOfNodes];
+        PriorityQueue<CompKruskalEdge> kruskalPQ = new PriorityQueue<CompKruskalEdge>();
+
+        for (int i = 0; i < nbrOfNodes; i++)
+            cc[i] = new LinkedList<E>();
+
+        for (int i = 0; i < edges.length; i++)
+        {
+            for (E temp : edges[i])
+            kruskalPQ.add(new CompKruskalEdge(temp));
+        }
+
+        while (!kruskalPQ.isEmpty() && cc.length > 1)
+        {
+            CompKruskalEdge nextEdge = kruskalPQ.poll();
+
+            LinkedList<E> longList,shortList;
+            if (cc[nextEdge.getSource()] !=(cc[nextEdge.getDest()]))
+            {
+                    if (cc[nextEdge.getSource()].size() > cc[nextEdge.getDest()].size() )
+                    {
+                        longList = cc[nextEdge.getSource()];
+                        shortList = cc[nextEdge.getDest()];
+                    }
+                    else {
+                        shortList = cc[nextEdge.getSource()];
+                        longList = cc[nextEdge.getDest()];
+                    }
+
+                    for (E temp : shortList){
+                        longList.add(temp);
+                    }
+
+                    longList.add((E) nextEdge.getEdge());
+                    for (E temp : longList){
+                        cc[temp.getSource()] = longList;
+                        cc[temp.getDest()] = longList;
+                    }
+                    if (longList.size() == nbrOfNodes-1)
+                        return longList .iterator();
+                }
+            }
+
+        return null;
+	}
 }
   
